d60b05180f7baa61f67940ae15ceeefc48fc820b
Ty Klabacka <klabacka.ty@gmail.com>
2023-07-02 17:47:15 -0600

    Added graph and began implementing algorithm

---
+++ graph.py
@@ -0,0 +1,89 @@
+import math   
+
+class Graph:
+    def __init__(self):
+        self.size = 0
+        self.vertList = {}
+
+    def add_vertex(self, label):
+        """Adds vertex to the graph, which is represented as a dictionary"""
+        if type(label) != str:
+            raise ValueError
+        new_vertex = Vertex(label)
+        self.vertList[label] = new_vertex
+        return self
+
+    def add_edge(self, src, dest, weight=0):
+        """Creates node if does not exist and connects it to another
+        node which will also be created if does not exist"""
+        if type(src) is not str and type(dest) is not str:
+            raise ValueError
+        try:
+            int(weight)
+        except ValueError:
+            raise ValueError
+        if src not in self.vertList:
+            self.add_vertex(src)
+        if dest not in self.vertList:
+            self.add_vertex(dest)
+        self.vertList[src].nbr_list[self.get_vertex(dest)] = weight
+        return self
+
+    def get_vertex(self, label):
+        """Returns the vertex object attached to parameter"""
+        if label in self.vertList:
+            return self.vertList[label]
+
+    def get_weight(self, src, dest):
+        """Returns weight between the two edges if path exists"""
+        if src in self.vertList and dest in self.vertList:
+            dest = self.get_vertex(dest)
+            try:
+                result = self.vertList[src].nbr_list[dest]
+            except KeyError:
+                return math.inf
+            return result
+        raise ValueError
+    
+    def __str__(self):
+        strang = 'digraph G {\n'
+        for item in self.vertList.values():
+            for nbr in item.get_edges():
+                weight = item.get_edges()[nbr]
+                strang += f'   {item.label} -> {nbr.label}' \
+                          f' [label="{weight:.1f}",weight="{weight:.1f}"];\n'
+        strang += '}\n'
+        return strang
+    
+    def shortest_travel_path(self, curVertex, curNum, path, home_vert):
+        
+        for node in curVertex.get_edges():
+            if node != home_vert or curNum >= self.size - 1:
+                print(node)
+
+    
+class Vertex:
+    def __init__(self, label, parent=None, color='white'):
+        self.nbr_list = {}
+        self.label = label
+        self.parent = parent
+        self.color = color
+
+    def add_neighbor(self, dest, weight):
+        self.nbr_list[dest] = weight
+
+    def reset(self):
+        self.parent = None
+        self.color = 'white'
+
+    def get_edges(self):
+        return self.nbr_list
+
+    def __str__(self):
+        return f'{self.label} is connected to {[item.label for item in self.nbr_list]}'
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+    def __gt__(self, other):
+        return self.distance > other.distance
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import os
+import json
+import re
+from graph import Graph, Vertex
 
 def get_directions(origin, destination, api_key):
     url = "https://maps.googleapis.com/maps/api/directions/json"
@@ -11,20 +14,67 @@ def get_directions(origin, destination, api_key):
     }
     response = requests.get(url, params=params)
     data = response.json()
-    print(data)
     if data["status"] == "OK":
         route = data["routes"][0]
         duration = route["legs"][0]["duration"]["text"]
         distance = route["legs"][0]["distance"]["text"]
-        steps = route["legs"][0]["steps"]
-        print(f"Total Distance: {distance}")
-        print(f"Total Duration: {duration}")
-        print("Steps:")
-        for i, step in enumerate(steps, 1):
-            print(f"Step {i}: {step['html_instructions']}")
+        # steps = route["legs"][0]["steps"]
+        # print(f"Total Distance: {distance}")
+        # print(f"Total Duration: {duration}")
+        # print("Steps:")
+        # for i, step in enumerate(steps, 1):
+        #     print(f"Step {i}: {step['html_instructions']}")
+    return duration, distance
     
 
-api_key = os.environ["google_maps_api_key"]
+
+
+def build_graph(api_key) -> Graph:
+    graph = Graph()
+
+    # Open the JSON file
+    with open("data.json") as file:
+        data = json.load(file)
+
+    # Iterate over the list of objects and access the addresses
+
+    addresses = [item["address"] for item in data]
+
+    with open("home_address.txt") as f:
+        addresses.append(f.readline())
+
+    for address in addresses:
+        for other_address in addresses:
+            if other_address != address:
+                data = get_directions(address, other_address, api_key)
+
+                duration_regex = r'(\d+)\s*(hour|min|second)s?'
+                matches = re.findall(duration_regex, data[0])
+                duration = 0
+                for match in matches:
+                    value, unit = match
+                    value = int(value)
+                    if unit == "hour":
+                        duration += value * 3600
+                    elif unit == "min":
+                        duration += value * 60
+                # print(data[0], duration)
+                graph.add_edge(address, other_address, duration)
+    return graph
+
+    
+
+def main():
+
+    api_key = os.environ["google_maps_api_key"]
+
+    print(build_graph(api_key))
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
